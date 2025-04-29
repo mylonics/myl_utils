2aab1f2f6fa9bf3224e91c0c0225039481b8744d
Rijesh Augustine <rijeshaugustine@gmail.com>
2025-04-29 14:01:09 -0600

    Added interrupt helpers

--- include/myl_utils/impl/hw_helper.h
+++ include/myl_utils/impl/hw_helper.h
@@ -46,6 +46,26 @@ static inline bool configure_and_check_successful(const struct gpio_dt_spec *spe
   if (configure_and_check_successful(&gpio, GPIO_INPUT)) \
     return 1;
 
+#define GPIO_INTERUPT_CB_HELPER(gpio, handler)                                              \
+  static struct gpio_callback gpio##_cb_data;                                               \
+  void gpio##_cb_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins) \
+  {                                                                                         \
+    handler();                                                                              \
+  }
+
+#define INIT_GPIO_INTERUPT_WITH_HANDLER(gpio, handler, edge)                                                 \
+  int gpio##_ret = gpio_pin_interrupt_configure_dt(&gpio, edge);                                             \
+  if (gpio##_ret != 0)                                                                                       \
+  {                                                                                                          \
+    printk("Error %d: failed to configure interrupt on %s pin %d\n", gpio##_ret, gpio.port->name, gpio.pin); \
+    return 1;                                                                                                \
+  }                                                                                                          \
+  gpio_init_callback(&gpio##_cb_data, handler, BIT(gpio.pin));                                               \
+  gpio_add_callback(gpio.port, &gpio##_cb_data);
+
+#define INIT_GPIO_INTERUPT(gpio, edge) \
+  INIT_GPIO_INTERUPT_WITH_HANDLER(gpio, gpio##_cb_handler, edge)
+
 #define GPIO_TOGGLE_FUNCTION(gpio) \
   void gpio##_toggle() { gpio_pin_toggle_dt(&gpio); }
 

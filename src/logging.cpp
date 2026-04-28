bafba99d16a490d069b2d5ce915719765a1589ab
Rijesh Augustine <rijeshaugustine@gmail.com>
2026-04-28 13:09:08 -0600

    Added mylutils namespace to logging.cpp

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,7 +1,10 @@
 
 #ifdef CONFIG_MYL_UTILS_LOG
 #include <myl_utils/zephyr/log.h>
-/* In all files comprising the module but one */
-// LOG_MODULE_REGISTER(myl_utils, CONFIG_MYL_UTILS_LOG_LEVEL);
+/* Register inside the myl_utils namespace so C++ headers that call
+ * DECLARE_MYL_UTILS_LOG() inside namespace myl_utils {} can resolve
+ * the log_const_myl_utils symbol without a namespace mismatch. */
+namespace myl_utils {
 INITIALIZE_MYL_UTILS_LOG();
+} // namespace myl_utils
 #endif
